4ac7964b11452ef925a0fde9213456e3e2a9e8ed
Tobias Raabe <raabe@posteo.de>
2020-06-25 16:20:25 +0200

    Fix version, etc.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 
 setup(
     name="pytask",
+    version="0.0.1",
     entry_points={"console_scripts": ["pytask=pytask.cli:pytask"]},
     packages=find_packages(where="src"),
     package_dir={"": "src"},
