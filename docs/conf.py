21aa60e6daae1848b0327a7a26ed407f4ed3b215
Tobias Raabe <raabe@posteo.de>
2020-07-22 11:33:43 +0200

    Release 0.0.4.

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ copyright = "2020, Tobias Raabe"  # noqa: A001
 author = "Tobias Raabe"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.3"
+release = "0.0.4"
 
 
 # -- General configuration -------------------------------------------------------------
--- src/pytask/__init__.py
+++ src/pytask/__init__.py
@@ -1,7 +1,7 @@
 import pluggy
 from pytask.mark import MARK_GEN as mark  # noqa: F401, N811
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 
 hookimpl = pluggy.HookimplMarker("pytask")
