199a114b7888e87a0b89948fdcb95bb43542d9cf
Tobias Raabe <raabe@posteo.de>
2020-08-12 15:33:33 +0200

    Real version bump.

--- src/pytask/__init__.py
+++ src/pytask/__init__.py
@@ -4,4 +4,4 @@ from _pytask.config import hookimpl
 from _pytask.mark import MARK_GEN as mark  # noqa: N811
 
 __all__ = ["hookimpl", "main", "mark", "pytask"]
-__version__ = "0.0.4"
+__version__ = "0.0.5"
