a22233da753965682b25b8292755a0b6ecc890d4
Tobias Raabe <raabe@posteo.de>
2020-08-16 13:47:51 +0200

    Add test.

---
+++ tests/test_logging.py
@@ -0,0 +1,20 @@
+import attr
+import pytest
+from _pytask.logging import _plugin_nameversions
+
+
+@attr.s
+class DummyDist:
+    project_name = attr.ib()
+    version = attr.ib()
+
+
+@pytest.mark.parametrize(
+    "plugins, expected",
+    [
+        ([(None, DummyDist("pytask-plugin", "0.0.1"))], ["plugin-0.0.1"]),
+        ([(None, DummyDist("plugin", "1.0.0"))], ["plugin-1.0.0"]),
+    ],
+)
+def test_plugin_nameversions(plugins, expected):
+    assert _plugin_nameversions(plugins) == expected
