28c4e046ff2a9fe63b7c2865dd903874c9546aff
Tobias Raabe <raabe@posteo.de>
2020-10-28 16:46:49 +0100

    Fix tests.

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -1,4 +1,3 @@
-import itertools
 import os
 import textwrap
 
@@ -8,17 +7,14 @@ from pytask import main
 
 @pytest.mark.end_to_end
 @pytest.mark.parametrize(
-    "dependencies, products",
-    itertools.product(
-        ([], ["in.txt"], ["in_1.txt", "in_2.txt"]),
-        (["out.txt"], ["out_1.txt", "out_2.txt"]),
-    ),
+    "dependencies",
+    [[], ["in.txt"], ["in_1.txt", "in_2.txt"]],
 )
+@pytest.mark.parametrize("products", [["out.txt"], ["out_1.txt", "out_2.txt"]])
 def test_execution_w_varying_dependencies_products(tmp_path, dependencies, products):
     source = f"""
     import pytask
     from pathlib import Path
-
 
     @pytask.mark.depends_on({dependencies})
     @pytask.mark.produces({products})
@@ -44,12 +40,10 @@ def test_depends_on_and_produces_can_be_used_in_task(tmp_path):
     import pytask
     from pathlib import Path
 
-
     @pytask.mark.depends_on("in.txt")
     @pytask.mark.produces("out.txt")
     def task_dummy(depends_on, produces):
         assert isinstance(depends_on, Path) and isinstance(produces, Path)
-
         produces.write_text(depends_on.read_text())
     """
     tmp_path.joinpath("task_dummy.py").write_text(textwrap.dedent(source))
