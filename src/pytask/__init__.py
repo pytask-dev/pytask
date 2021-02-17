1af224a4356e3fd4ef0f2168f4f46e8ef1c1f611
Tobias Raabe <raabe@posteo.de>
2021-02-17 23:09:19 +0100

    Some small fixes.

--- src/pytask/__init__.py
+++ src/pytask/__init__.py
@@ -3,10 +3,9 @@ from _pytask.cli import cli
 from _pytask.config import hookimpl
 from _pytask.mark import MARK_GEN as mark  # noqa: N811
 
-__all__ = ["cli", "hookimpl", "main", "mark"]
-__version__ = "0.0.11"
-
 from ._version import get_versions
+
+__all__ = ["cli", "hookimpl", "main", "mark"]
 
 __version__ = get_versions()["version"]
 del get_versions
