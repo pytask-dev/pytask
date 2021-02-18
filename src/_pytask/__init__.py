e698d2b58f719fb02762d46ba56153a1bb4aaa44
Tobias Raabe <raabe@posteo.de>
2021-02-18 23:23:44 +0100

    Fix version for CI.

--- src/_pytask/__init__.py
+++ src/_pytask/__init__.py
@@ -1,5 +1,10 @@
+import os
+
 from ._version import get_versions
 
 __version__ = get_versions()["version"]
+if "CI" in os.environ:
+    __version__ = __version__.split("+")[0]
+
 __git_version__ = get_versions()["full-revisionid"]
 del get_versions
