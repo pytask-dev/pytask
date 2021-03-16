39fe37bf90f5b38be4023e549eeb857c276d595c
Tobias Raabe <raabe@posteo.de>
2021-03-16 23:06:49 +0100

    Fix docs.

--- src/_pytask/console.py
+++ src/_pytask/console.py
@@ -4,9 +4,10 @@ import sys
 from rich.console import Console
 
 
-_COLOR_SYSTEM = "auto"
 if sys.platform == "win32" and "WT_SESSION" not in os.environ:
     _COLOR_SYSTEM = None
+else:
+    _COLOR_SYSTEM = "auto"
 
 
 console = Console(color_system=_COLOR_SYSTEM)
