8fa421c9d5731383b3c6d6d456822ffd83e5170b
Tobias Raabe <raabe@posteo.de>
2021-07-08 00:34:10 +0200

    Make the table during execution the default.

--- src/_pytask/mark_utils.py
+++ src/_pytask/mark_utils.py
@@ -1,8 +1,17 @@
+"""This module contains utility functions related to marker.
+
+The utility functions are stored here to be separate from the plugin.
+
+"""
+
+
 def get_specific_markers_from_task(task, marker_name):
+    """Get a specific group of markers from a task."""
     return [marker for marker in task.markers if marker.name == marker_name]
 
 
 def get_marks_from_obj(obj, marker_name):
+    """Get a specific group of markers from a task function."""
     return [
         marker
         for marker in getattr(obj, "pytaskmark", [])
@@ -11,4 +20,5 @@ def get_marks_from_obj(obj, marker_name):
 
 
 def has_marker(obj, marker_name):
+    """Determine whether a task function has a certain marker."""
     return any(marker.name == marker_name for marker in getattr(obj, "pytaskmark", []))
