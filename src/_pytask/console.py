2ad41c274ca9ec895907644c009958857efab886
Tobias Raabe <raabe@posteo.de>
2021-12-26 21:23:43 +0100

    link execution.

--- src/_pytask/console.py
+++ src/_pytask/console.py
@@ -9,8 +9,10 @@ from typing import Callable
 from typing import Dict
 from typing import Iterable
 from typing import TYPE_CHECKING
+from typing import Union
 
 from rich.console import Console
+from rich.style import Style
 from rich.theme import Theme
 from rich.tree import Tree
 
@@ -83,7 +85,7 @@ def escape_squared_brackets(string: str) -> str:
     return string.replace("[", "\\[")
 
 
-def create_url_style_for_task(task: "MetaTask", edtior_url_scheme: str) -> str:
+def create_url_style_for_task(task: "MetaTask", edtior_url_scheme: str) -> Style:
     """Create the style to add a link to a task id."""
     url_scheme = _EDITOR_URL_SCHEMES.get(edtior_url_scheme, edtior_url_scheme)
 
@@ -92,7 +94,7 @@ def create_url_style_for_task(task: "MetaTask", edtior_url_scheme: str) -> str:
         "line_number": inspect.getsourcelines(task.function)[1],
     }
 
-    return "" if not url_scheme else "link " + url_scheme.format(**info)
+    return Style() if not url_scheme else Style(link=url_scheme.format(**info))
 
 
 def _get_file(function: Callable[..., Any]) -> Path:
@@ -101,3 +103,10 @@ def _get_file(function: Callable[..., Any]) -> Path:
         return _get_file(function.func)
     else:
         return Path(inspect.getfile(function))
+
+
+def unify_styles(*styles: Union[str, Style]) -> Style:
+    """Unify styles."""
+    return Style.combine(
+        Style.parse(style) if isinstance(style, str) else style for style in styles
+    )
--- tests/test_console.py
+++ tests/test_console.py
@@ -5,6 +5,7 @@ import attr
 import pytest
 from _pytask.console import create_url_style_for_task
 from _pytask.nodes import MetaTask
+from rich.style import Style
 
 
 def task_func():
@@ -36,9 +37,8 @@ _SOURCE_LINE_TASK_FUNC = inspect.getsourcelines(task_func)[1]
         ("vscode", f"link vscode://file/{{path}}:{_SOURCE_LINE_TASK_FUNC}"),
         ("pycharm", f"link pycharm://open?file={{path}}&line={_SOURCE_LINE_TASK_FUNC}"),
         (
-            f"imaginary-editor://module={{path}}&line_number={_SOURCE_LINE_TASK_FUNC}",
-            "link imaginary-editor://module={{path}}&"
-            f"line_number={_SOURCE_LINE_TASK_FUNC}",
+            f"editor://module={{path}}&line_number={_SOURCE_LINE_TASK_FUNC}",
+            f"link editor://module={{path}}&line_number={_SOURCE_LINE_TASK_FUNC}",
         ),
     ],
 )
@@ -46,4 +46,4 @@ def test_create_url_style_for_task(edtior_url_scheme, expected):
     path = Path(__file__)
     task = DummyTask(task_func)
     style = create_url_style_for_task(task, edtior_url_scheme)
-    assert style == expected.format(path=path)
+    assert style == Style.parse(expected.format(path=path))
