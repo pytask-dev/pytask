26a383af1cb73cd1b2b25de81199ee6014297df0
Tobias Raabe <raabe@posteo.de>
2022-01-18 23:23:05 +0100

    Add objects to fully type pytask_parallel.

--- src/pytask/__init__.py
+++ src/pytask/__init__.py
@@ -5,6 +5,17 @@ from _pytask.cli import cli
 from _pytask.config import hookimpl
 from _pytask.graph import build_dag
 from _pytask.mark import MARK_GEN as mark  # noqa: N811
+from _pytask.nodes import MetaTask
+from _pytask.session import Session
 
 
-__all__ = ["__version__", "build_dag", "cli", "hookimpl", "main", "mark"]
+__all__ = [
+    "__version__",
+    "build_dag",
+    "cli",
+    "hookimpl",
+    "main",
+    "mark",
+    "MetaTask",
+    "Session",
+]
