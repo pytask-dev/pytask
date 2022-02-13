19422372956adcb2267d0a9919bb70b2bcbdc304
Tobias Raabe <raabe@posteo.de>
2022-02-13 23:16:28 +0100

    publish more.

--- src/pytask/__init__.py
+++ src/pytask/__init__.py
@@ -1,6 +1,9 @@
 """This module contains the main namespace for pytask."""
 from __future__ import annotations
 
+from _pytaks.mark_utils import get_specific_markers_from_task
+from _pytaks.mark_utils import has_marker
+from _pytaks.mark_utils import remove_markers_from_func
 from _pytask import __version__
 from _pytask.build import main
 from _pytask.cli import cli
@@ -21,9 +24,12 @@ __all__ = [
     "__version__",
     "build_dag",
     "cli",
+    "get_specific_markers_from_task",
+    "has_marker",
     "hookimpl",
     "main",
     "mark",
+    "remove_markers_from_func",
     "FilePathNode",
     "Mark",
     "MarkDecorator",
