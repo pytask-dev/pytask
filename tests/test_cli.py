a67c9246fca6927ca5ba45b2a035db6ec3445699
Tobias Raabe <raabe@posteo.de>
2022-12-16 12:11:16 +0100

    Defaults in help texts are overwritten by config.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -31,3 +31,16 @@ def test_version_option():
 def test_help_pages(runner, commands, help_option):
     result = runner.invoke(cli, [*commands, help_option])
     assert result.exit_code == ExitCode.OK
+
+
+def test_help_texts_are_modified_by_config(runner, tmp_path):
+    tmp_path.joinpath("pyproject.toml").write_text(
+        '[tool.pytask.ini_options]\nshow_capture = "stdout"'
+    )
+
+    result = runner.invoke(
+        cli,
+        ["build", "--help", "--config", tmp_path.joinpath("pyproject.toml").as_posix()],
+    )
+
+    assert "[default: stdout]" in result.output
