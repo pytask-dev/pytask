67e743bc897a02647fa9c9505f06eae858f9c4fc
Tobias Raabe <raabe@posteo.de>
2023-03-05 17:01:54 +0100

    Fix.

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -388,13 +388,9 @@ def test_that_dynamically_creates_tasks_are_captured(runner, tmp_path):
 @pytest.mark.end_to_end()
 def test_task_executed_with_force_although_unchanged(tmp_path):
     tmp_path.joinpath("task_module.py").write_text("def task_example(): pass")
-
     session = main({"paths": tmp_path})
-
     assert session.execution_reports[0].outcome == TaskOutcome.SUCCESS
-
     session = main({"paths": tmp_path, "force": True})
-
     assert session.execution_reports[0].outcome == TaskOutcome.SUCCESS
 
 
