35affb351dce6a86755b63bbca02221888a794ba
Tobias Raabe <raabe@posteo.de>
2023-07-29 21:10:12 +0200

    Add another test.

--- tests/test_collect_command.py
+++ tests/test_collect_command.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+import pickle
 import textwrap
 from pathlib import Path
 
@@ -517,3 +518,42 @@ def test_python_nodes_are_aggregated_into_one(runner, tmp_path):
     assert "task_example>" in captured
     assert "<Dependency nested>" in result.output
     assert "Product" in captured
+
+
+def test_node_protocol_for_custom_nodes_with_paths(runner, tmp_path):
+    source = """
+    from typing_extensions import Annotated
+    from pytask import Product
+    from pathlib import Path
+    from attrs import define
+    import pickle
+
+    @define
+    class PickleFile:
+        name: str
+        path: Path
+
+        @property
+        def value(self):
+            with self.path.open("rb") as f:
+                out = pickle.load(f)
+            return out
+
+        def state(self):
+            return str(self.path.stat().st_mtime)
+
+
+    _PATH = Path(__file__).parent.joinpath("in.pkl")
+
+    def task_example(
+        data = PickleFile(_PATH.as_posix(), _PATH),
+        out: Annotated[Path, Product] = Path("out.txt"),
+    ) -> None:
+        out.write_text(data)
+    """
+    tmp_path.joinpath("task_module.py").write_text(textwrap.dedent(source))
+    tmp_path.joinpath("in.pkl").write_bytes(pickle.dumps("text"))
+
+    result = runner.invoke(cli, ["collect", "--nodes", tmp_path.as_posix()])
+    assert result.exit_code == ExitCode.OK
+    assert "in.pkl" in result.output
