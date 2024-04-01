pytask-dev/pytask

commit ae7231de006d8b67d26433ae041e51a86412b85b

Reset class variables of `ExecutionReport` and `Traceback`. (#588)

src/_pytask/traceback.py
@@ -13,6 +13,7 @@ from typing import Union
 
 import pluggy
 from attrs import define
+from attrs import field
 from rich.traceback import Traceback as RichTraceback
 
 import _pytask
@@ -45,13 +46,18 @@ OptionalExceptionInfo: TypeAlias = Union[ExceptionInfo, Tuple[None, None, None]]
 @define
 class Traceback:
     exc_info: OptionalExceptionInfo
+    show_locals: bool = field()
 
-    show_locals: ClassVar[bool] = False
+    _show_locals: ClassVar[bool] = False
     suppress: ClassVar[tuple[Path, ...]] = (
         _PLUGGY_DIRECTORY,
-        TREE_UTIL_LIB_DIRECTORY,
         _PYTASK_DIRECTORY,
+        TREE_UTIL_LIB_DIRECTORY,
     )
+
+    @show_locals.default
+    def _show_locals_default(self) -> bool:
+        return self._show_locals
 
     def __rich_console__(
         self, console: Console, console_options: ConsoleOptions
@@ -68,7 +74,7 @@ class Traceback:
             yield filtered_exc_info[2]
         else:
             yield RichTraceback.from_exception(
-                *filtered_exc_info, show_locals=self.show_locals
+                *filtered_exc_info, show_locals=self._show_locals
             )
 
 

src/_pytask/logging.py
@@ -15,6 +15,7 @@ import pluggy
 from rich.text import Text
 
 import _pytask
+from _pytask.capture_utils import ShowCapture
 from _pytask.console import IS_WINDOWS_TERMINAL
 from _pytask.console import console
 from _pytask.pluginmanager import hookimpl
@@ -67,7 +68,7 @@ def pytask_parse_config(config: dict[str, Any]) -> None:
 @hookimpl
 def pytask_post_parse(config: dict[str, Any]) -> None:
     # Set class variables on traceback object.
-    Traceback.show_locals = config["show_locals"]
+    Traceback._show_locals = config["show_locals"]
     # Set class variables on Executionreport.
     ExecutionReport.editor_url_scheme = config["editor_url_scheme"]
     ExecutionReport.show_capture = config["show_capture"]
@@ -122,6 +123,15 @@ def pytask_log_session_footer(
         f"{outcome.description} in {formatted_duration}", style=outcome.style
     )
     console.rule(message, style=outcome.style)
+
+
+@hookimpl
+def pytask_unconfigure() -> None:
+    """Reset class variables."""
+    Traceback._show_locals = False
+    ExecutionReport.editor_url_scheme = "file"
+    ExecutionReport.show_capture = ShowCapture.ALL
+    ExecutionReport.show_locals = False
 
 
 _TIME_UNITS: list[_TimeUnit] = [

tests/test_traceback.py
@@ -50,3 +50,13 @@ def test_render_traceback_with_string_traceback():
     traceback = Traceback((Exception, Exception("Help"), "String traceback."))
     rendered = render_to_string(traceback, console)
     assert "String traceback." in rendered
+
+
+@pytest.mark.unit()
+def test_passing_show_locals():
+    traceback = Traceback(
+        (Exception, Exception("Help"), "String traceback."), show_locals=True
+    )
+    assert traceback.show_locals is True
+    # Also tests that the class variable has been reset.
+    assert traceback._show_locals is False

commit 5df5fa1cdf2ba69a278a8d01545185231f9ecee6

Fix error introduced by #588. (#590)

src/_pytask/traceback.py
@@ -74,7 +74,7 @@ class Traceback:
             yield filtered_exc_info[2]
         else:
             yield RichTraceback.from_exception(
-                *filtered_exc_info, show_locals=self._show_locals
+                *filtered_exc_info, show_locals=self.show_locals
             )
 
 