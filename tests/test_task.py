31cbcebb9fdb02701f4b478321dbbcdf267b4d74
Tobias Raabe <raabe@posteo.de>
2024-11-02 19:50:04 +0100

    Fix.

--- tests/test_task.py
+++ tests/test_task.py
@@ -689,7 +689,10 @@ def test_task_will_be_executed_after_another_one_with_function_session(
     """
     tmp_path.joinpath("task_example.py").write_text(textwrap.dedent(source))
 
-    result = subprocess.run(("python",), cwd=tmp_path, capture_output=True, check=False)
+    result = subprocess.run(
+        ("python", "task_example.py"), cwd=tmp_path, capture_output=True, check=False
+    )
+    assert "2  Succeeded" in result.stdout.decode()
     assert result.returncode == ExitCode.OK
 
 
