b6a57af549a6574ac977a54e7f098cae9ff0e25e
Tobias Raabe <raabe@posteo.de>
2025-04-13 18:21:49 +0200

    Fix documentation on complex repetitions.

--- docs_src/how_to_guides/bp_complex_task_repetitions/example_improved.py
+++ docs_src/how_to_guides/bp_complex_task_repetitions/example_improved.py
@@ -2,13 +2,16 @@ from typing import Annotated
 from typing import Any
 
 from myproject.config import EXPERIMENTS
+from myproject.config import Model
 from myproject.config import data_catalog
 
+from _pytask.nodes import PythonNode
 from pytask import task
 
 for experiment in EXPERIMENTS:
 
     @task(id=experiment.name)
     def task_fit_model(
+        model: Annotated[Model, PythonNode(value=experiment.model, hash=True)],
         path_to_data: experiment.dataset.path,
     ) -> Annotated[Any, data_catalog[experiment.fitted_model_name]]: ...
