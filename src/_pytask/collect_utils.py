f4686b44702637392c50af851c9cbe3e388db7c3
Tobias Raabe <raabe@posteo.de>
2026-09-20 11:08:33 +0000

    fix: disallow products on task generators (#996)

--- src/_pytask/collect_utils.py
+++ src/_pytask/collect_utils.py
@@ -156,8 +156,16 @@ def task_example() -> str:
 Read more about products in the documentation: http://tinyurl.com/pytask-return.
 """
 
+_ERROR_TASK_GENERATOR_PRODUCTS = """Task generators cannot define products with a
+'produces' function argument.
+"""
 
-def parse_products_from_task_function(
+_ERROR_TASK_GENERATOR_RETURN_ANNOTATION = """Task generators cannot define products
+with a return annotation.
+"""
+
+
+def parse_products_from_task_function(  # noqa: C901
     session: Session, task_path: Path | None, task_name: str, node_path: Path, obj: Any
 ) -> dict[str, Any]:
     """Parse products from task function.
@@ -171,6 +179,7 @@ def parse_products_from_task_function(
     """
     has_return = False
     has_task_decorator = False
+    is_generator = isinstance(obj, TaskFunction) and obj.pytask_meta.is_generator
 
     out: dict[str, Any] = {}
 
@@ -182,11 +191,23 @@ def parse_products_from_task_function(
     parameters_with_product_annot = _find_args_with_product_annotation(obj)
     parameters_with_node_annot = _find_args_with_node_annotation(obj)
 
+    if is_generator and "produces" in parameters:
+        raise NodeNotCollectedError(_ERROR_TASK_GENERATOR_PRODUCTS)
+
+    if is_generator:
+        if "return" in parameters_with_node_annot:
+            raise NodeNotCollectedError(_ERROR_TASK_GENERATOR_RETURN_ANNOTATION)
+
+        parameters_with_product_annot = [
+            name for name in parameters_with_product_annot if name != "return"
+        ]
+        parameters_with_node_annot.pop("return", None)
+
     # Allow to collect products from 'produces'.
     if "produces" in parameters and "produces" not in parameters_with_product_annot:
         parameters_with_product_annot.append("produces")
 
-    if "return" in parameters_with_node_annot:
+    if not is_generator and "return" in parameters_with_node_annot:
         parameters_with_product_annot.append("return")
         has_return = True
 
@@ -227,7 +248,7 @@ def parse_products_from_task_function(
             out[parameter_name] = collected_products
 
     task_produces = obj.pytask_meta.produces if isinstance(obj, TaskFunction) else None
-    if task_produces:
+    if task_produces and not is_generator:
         has_task_decorator = True
         collected_products = _collect_nodes_and_provisional_nodes(
             _collect_product,
